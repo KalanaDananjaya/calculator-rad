6acda1066c80c367fd7e676e6d021c0521e39f17
Kalana Wijethunga <kalana.16@cse.mrt.ac.lk>
2018-09-12 15:15:58 +0530

    added team member details

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,25 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private firstNum : string;
+  private secondNum : string;
+  private finalRes: string;
+  private a: number;
+  private b: number;
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  addNum(){
+    
+    this.a=(parseInt(this.firstNum)+parseInt(this.secondNum));
+    alert(this.a);
+  }
+
+  minNum(){
+    this.b=(parseInt(this.firstNum)-parseInt(this.secondNum));
+    alert(this.b);
   }
 
 }
